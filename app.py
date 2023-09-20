5b280ca48ea794d3f6f1176abcc25b91dacaa3f3
sallyzy0123 <sally_zy@protonmail.com>
2023-09-21 02:00:43 +0300

    done with translator web app with Flask framework

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from transformers import pipeline
+
+app = Flask(__name__)
+pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-fi")
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    translation = None
+    if request.method == 'POST':
+        text = request.form ['content']
+        translation = pipe(text, max_length=100)[0]['translation_text']
+        return render_template('index.html', text=text, translation=translation)
+    else:
+        return render_template('index.html')
+    
+if __name__ == "__main__":
+    app.run(debug=True)
